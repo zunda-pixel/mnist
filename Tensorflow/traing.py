259b998bd96b396ce1d7e4f26959e2378c085623
zunda <47569369+zunda-pixel@users.noreply.github.com>
2021-02-08 00:14:36 +0900

    Update traing.py

--- Tensorflow/traing.py
+++ Tensorflow/traing.py
@@ -14,7 +14,6 @@ else:
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
 x_test  = x_test.reshape(x_test.shape[0],   x_test.shape[1]  * x_test.shape[2])
 
