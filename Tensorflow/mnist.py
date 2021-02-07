8af26a1345cc915d04cf7dd1d979dd564b7bef4c
zunda <47569369+zunda-pixel@users.noreply.github.com>
2021-02-08 00:11:49 +0900

    Update mnist.py

--- Tensorflow/mnist.py
+++ Tensorflow/mnist.py
@@ -1,1 +1,20 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import load_model
 
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_test  = x_test.reshape(x_test.shape[0], x_test.shape[1] * x_test.shape[2])
+
+x_test  = x_test.astype('float32')
+
+x_test  /= 255
+
+num_classes = len(list(set(y_test)))
+
+y_test = to_categorical(y_test, num_classes)
+
+model = load_model('widgets.h5')
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
